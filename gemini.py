8ee1d42e79e40599d7fb7875e0127d9db57152f3
syd <167946750+sytSmit@users.noreply.github.com>
2025-04-05 12:00:59 -0500

    changed prompt

--- gemini.py
+++ gemini.py
@@ -13,7 +13,7 @@ class Gemini:
       #  model="gemini-2.0-flash", contents=f"Give me JUST the estimated cost of ingredients for {foods} in a numbered list"
       #  )
         response = self._client.models.generate_content(                                                     #eventually should be for {user_food} or smthn
-        model="gemini-2.0-flash", contents=f"Give me JUST the estimated cost of ingredients for {foods}, combine ingredients and list them all out in a numbered list with cost, make sure to number the ingredients"
+        model="gemini-2.0-flash", contents=f"Give me JUST the estimated cost of ingredients for {foods}, combine ingredients and list them all out in a numbered list with cost, make sure to number the ingredients. This list should be optimized so that only the minimum number of ingredients are listed. List all ingredients in one list, do NOT separate them by meal. The formatting for one ingredient should look like this: 1. *swiss cheese*: $6.00. If multiples of an item are needed, "
         )
 
         print(response.text)
@@ -22,5 +22,10 @@ class Gemini:
     
 
    
+'''
+Give me JUST the estimated cost of ingredients for {foods}, 
+combine ingredients and list them all out in a numbered list with cost,
+make sure to number the ingredients. This list should be optimized so that only the minimum number of ingredients are listed. List all ingredients in one list, do NOT separate them by meal. The formatting for one item should look like this: 1. *swiss cheese*: $6.00
     
+'''
   #  def list_of_ing(self, food):
