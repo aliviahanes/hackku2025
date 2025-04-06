ff03d68bd15ff484a5b1c1938d21985d0e179016
Bluuto05 <luciaulate05@gmail.com>
2025-04-06 02:01:02 -0500

    task lists work on planner!

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route("/<name>")
-def welcome(name):
-    return render_template("welcome.html", name=name)
+@app.route("/planner")
+def planner():
+    return render_template("planner.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
