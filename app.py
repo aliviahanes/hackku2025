b4f0d526ee4e92252c6c28adb8494b7f6b8c3fe5
Bluuto05 <luciaulate05@gmail.com>
2025-04-05 21:16:00 -0500

    flaks works! yipee

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/<name>")
+def welcome(name):
+    return render_template("welcome.html", name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
