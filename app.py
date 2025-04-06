2fc51b6b131890e0574fcdeaf4f48ad4aa5ef65f
Bluuto05 <luciaulate05@gmail.com>
2025-04-06 03:13:41 -0500

    flask update # idek

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('planner.html')
 
 @app.route("/planner")
 def planner():
